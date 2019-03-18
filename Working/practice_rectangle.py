e8ec7c869cf4e15f5aaaf27017487c9b9ea052d0
FortinM <robuntu@stro.ycdsb.ca>
2019-03-18 09:49:16 -0400

    class methods

---
+++ Working/practice_rectangle.py
@@ -0,0 +1,20 @@
+class Rectangle(object):
+
+    def __init__(self):
+        self.width = 0
+        self.height = 0
+
+def get_area()
+
+def main():
+
+    rect1 = Rectangle()
+    rect1.width = input()
+    rect1.height = input()
+
+    print("The Width of the rectangle is " + width)
+    print("The height of the rectangle is " + height)
+    print("The area of the reactangle is " + (width*height))
+
+
+main()
